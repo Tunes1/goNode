7ca6c506db580c14f6dcf6fda1cff07a4171aad5
Rtunes1 <ricardotuva21@hotmail.com>
2019-01-13 17:45:22 -0200

    server js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 class App {
     constructor(){
-        
+        this.express = express();
+        this.isDev = process.env.NODE_ENV != 'production'
+        this.middlewares();
+        this.views();
+        this.routes();
+    }
+
+    middlewares(){
+        this.express.use(express.urlencoded({ extended: false}))
+    }
+
+    views(){
+
+
+    }
+
+    routes(){
+
+
     }
 }
